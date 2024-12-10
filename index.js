01f8f0569bceca3669263c03ef3878298eb14b85
rochellejjsmith <150965089+rochellejjsmith@users.noreply.github.com>
2024-12-10 14:39:01 +0000

    Update index.js
    local host 8080

--- index.js
+++ index.js
@@ -28,5 +28,5 @@ io.on("connection", (socket) => {
    });
 });
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
 httpServer.listen(PORT, () => console.log(`Server running on port ${PORT}`));
