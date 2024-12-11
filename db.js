acb0d93c6368716904478005ba70a881eb0dd959
rochellejjsmith <150965089+rochellejjsmith@users.noreply.github.com>
2024-12-11 14:38:33 +0000

    Update db.js
    dynamo db

--- db.js
+++ db.js
@@ -1,20 +1,13 @@
-const mysql = require('mysql2');
+const AWS = require('aws-sdk');
 
-// Create a connection to the database
-const db = mysql.createConnection({
-  host: 'database-1.cluster-c90gc4uuyu3f.eu-west-2.rds.amazonaws.com', // Database endpoint
-  user: 'admin1',                                                     // Database username
-  password: 'Renzmunii.13',                                           // Database password
-  database: 'database-1'                                              // Database name
+// Configure AWS DynamoDB
+AWS.config.update({
+  region: 'eu-west-2', // Replace with your region
+  accessKeyId: 'YOUR_AWS_ACCESS_KEY_ID', // Replace with your AWS Access Key ID
+  secretAccessKey: 'YOUR_AWS_SECRET_ACCESS_KEY' // Replace with your AWS Secret Access Key
 });
 
-// Connect to the database
-db.connect((err) => {
-  if (err) {
-    console.error('Database connection failed:', err.stack);
-    return;
-  }
-  console.log('Connected to database');
-});
+// Create a DynamoDB DocumentClient
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-module.exports = db;
+module.exports = dynamoDb;
